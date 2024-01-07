cdba3a697163a8c6eaf50b41695e4b23a5e58e6c
Jefferson Duarte <duartejefferson123@gmail.com>
2024-01-07 17:29:50 -0300

    :sparkles: feat: Adicionando funcionalidades no formulário de cadastro de tarefas

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -1,22 +1,45 @@
 import styles from "./TaskForm.module.css"
 
+//Components
+import { useState, useEffect, ChangeEvent,FormEvent } from "react"
+
+//Interface
+import { ITask } from "../../interfaces/Task"
+
 interface Props {
   btnText: string
 }
 
 const TaskForm = ({btnText}: Props) => {
+
+  const [id, setId] = useState<number>(0)
+  const [title, setTitle] = useState<string>("")
+  const [difficulty, setDifficulty] = useState<number>(0)
+
+  const addTaskHandler = () => {}
+
+  const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+    if(e.target.name === "title"){
+      setTitle(e.target.value)
+    }else{
+      setDifficulty(parseInt(e.target.value))
+    }
+    console.log(title)
+    console.log(difficulty)
+  }
+
   return (
     <form className={styles.form}>
-      <div className={styles.input_container}>
+      <div onSubmit={addTaskHandler} className={styles.input_container}>
         <label>
           Título:
-          <input type="text" placeholder="Título da tarefa" />
+          <input type="text" placeholder="Título da tarefa" onChange={handleChange} name="title" />
         </label>
       </div>
       <div className={styles.input_container}>
         <label>
           Dificuldade:
-          <input type="text" placeholder="Dificuldade da tarefa" />
+          <input type="text" placeholder="Dificuldade da tarefa" onChange={handleChange} />
         </label>
       </div>
       <input type="submit" value={btnText} />
