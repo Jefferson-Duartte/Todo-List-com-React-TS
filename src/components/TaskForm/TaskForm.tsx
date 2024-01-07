6c10fd57b7871aeaf782a1c27cda4117b4378383
Jefferson Duarte <duartejefferson123@gmail.com>
2024-01-07 15:10:29 -0300

    :lipstick: feat: Estilização do formulário de cadastro de nova tarefa

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -1,3 +1,4 @@
+import styles from "./TaskForm.module.css"
 
 interface Props {
   btnText: string
@@ -5,14 +6,14 @@ interface Props {
 
 const TaskForm = ({btnText}: Props) => {
   return (
-    <form>
-      <div>
+    <form className={styles.form}>
+      <div className={styles.input_container}>
         <label>
           Título:
           <input type="text" placeholder="Título da tarefa" />
         </label>
       </div>
-      <div>
+      <div className={styles.input_container}>
         <label>
           Dificuldade:
           <input type="text" placeholder="Dificuldade da tarefa" />
