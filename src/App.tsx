d39e870c939cb510def633c2b6c359e477e1ce4a
Jefferson Duarte <duartejefferson123@gmail.com>
2024-01-07 14:03:52 -0300

    :tada: Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <h2>React + TS To-do</h2>
+    </header>
+    <h1>Conteudo...</h1>
+
+    <footer>
+      <p>
+        <span>React + TS To-do</span> @2024
+      </p>
+    </footer>
+    </>
+  )
+}
+
+export default App
