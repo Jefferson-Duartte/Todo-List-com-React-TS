348a29ee151ab9e0449c5146724b4203c47f6612
Jefferson Duarte <duartejefferson123@gmail.com>
2024-01-07 22:27:22 -0300

    :sparkles: feat: Exibindo HTML dos itens da lista

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,8 +1,27 @@
+import { ITask } from '../../interfaces/Task'
+import styles from "./TaskList.module.css"
 
+type Props = {
+  taskList: ITask[]
+}
 
-const TaskList = () => {
+const TaskList = ({taskList}: Props) => {
   return (
-    <div><p>List</p></div>
+    <>
+    {taskList.length > 0 ? (taskList.map((task) => (
+      <div key={task.id}>
+        <div>
+          <h4>{task.title}</h4>
+          <p>{task.difficulty}</p>
+          </div>
+        <div>
+          <i className='bi bi-pencil'></i>
+          <i className='bi bi-trash'></i>
+        </div>
+      </div>
+    ))) : (<p>Não há tarefas cadastradas</p>) }
+
+    </>
   )
 }
 
